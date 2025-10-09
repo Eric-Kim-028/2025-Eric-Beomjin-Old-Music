38bab2b26ec071de90b0555079e3407ba45b30f9
Eric-Kim-028 <2011erickim@gmail.com>
2025-10-09 00:55:03 +0000

    artists page added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,7 @@
 <div class="flex space-x-5 border-b p-2">
 	<a href="/">Home</a>
 	<a href="/favorites">Fav</a>
+	<a href="/Artists">Artists</a>
 </div>
 
 {@render children?.()}
---
+++ src/routes/Artists/+page.svelte
@@ -0,0 +1,52 @@
+<center class="size">Users section</center>
+<div class="margin"></div>
+<center class="size"
+	>Search:<input placeholder="Search up artists :)" type="text" class="border" /></center
+>
+<div class="margin"></div>
+<div class="margin"></div>
+<center class="size">Recommended artists:</center>
+<div class="justify-evenly">
+	<div class="height width border"></div>
+	<div class="height width border"></div>
+	<div class="height width border"></div>
+	<div class="height width border"></div>
+</div>
+<div class="margin"></div>
+<center class="size">Followed artists:</center>
+<div class="justify-evenly">
+	<div class="height width border"></div>
+	<div class="height width border"></div>
+	<div class="height width border"></div>
+	<div class="height width border"></div>
+</div>
+<div class="margin"></div>
+<center class="size">Most followed artists:</center>
+<div class="justify-evenly">
+	<div class="height width border"></div>
+	<div class="height width border"></div>
+	<div class="height width border"></div>
+	<div class="height width border"></div>
+</div>
+
+<style>
+	.size {
+		font-size: 30px;
+	}
+	.margin {
+		margin: 50px;
+	}
+	.justify-evenly {
+		display: flex;
+		justify-content: space-evenly;
+	}
+	.height {
+		height: 32px;
+	}
+	.width {
+		width: 48px;
+	}
+	.border {
+		border: 1px solid black;
+	}
+</style>
