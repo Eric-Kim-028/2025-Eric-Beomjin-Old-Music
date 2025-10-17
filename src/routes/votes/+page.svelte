d6d42d7e4f0bed9f2e0cbe5e46f1ffcdf18308c1
Eric-Kim-028 <2011erickim@gmail.com>
2025-10-17 23:01:18 +0000

    change in file

--- src/routes/votes/+page.svelte
+++ src/routes/votes/+page.svelte
@@ -2,51 +2,51 @@
 	let arr = [
 		// 🎸 1980s
 		{
-			artist: 'Michael Jackson',
-			name: 'Billie Jean',
-			src: 'https://www.youtube.com/embed/Zi_XLOBDo_Y?si=Zt1FPG8Q9QKX3FJg',
+			artist: 'Whitney Houston',
+			name: 'I Wanna Dance with Somebody',
+			src: 'https://www.youtube.com/embed/eH3giaIzONA',
 			liked: false,
 			era: '1980s'
 		},
 		{
-			artist: 'Nirvana',
-			name: 'Smells Like Teen Spirit',
-			src: 'https://www.youtube.com/embed/hTWKbfoikeg?si=2azDHQiaIG7kSB6y',
+			artist: 'Prince',
+			name: 'Purple Rain',
+			src: 'https://www.youtube.com/embed/TvnYmWpD_T8',
+			liked: false,
+			era: '1980s'
+		},
+		{
+			artist: 'Madonna',
+			name: 'Like a Prayer',
+			src: 'https://www.youtube.com/embed/79fzeNUqQbQ',
+			liked: false,
+			era: '1980s'
+		},
+		{
+			artist: 'TLC',
+			name: 'No Scrubs',
+			src: 'https://www.youtube.com/embed/FrLequ6dUdM',
 			liked: false,
 			era: '1990s'
 		},
 		{
-			artist: 'Oasis',
-			name: 'Wonderwall',
-			src: 'https://www.youtube.com/embed/bx1Bh8ZvH84?si=7D5xZyN3GtxGrxkF',
+			artist: 'George Michael',
+			name: 'Faith',
+			src: 'https://www.youtube.com/embed/6Cs3Pvmmv0E',
+			liked: false,
+			era: '1980s'
+		},
+		{
+			artist: 'Radiohead',
+			name: 'Creep',
+			src: 'https://www.youtube.com/embed/XFkzRNyygfk',
 			liked: false,
 			era: '1990s'
 		},
 		{
-			artist: 'Queen',
-			name: 'Another One Bites the Dust',
-			src: 'https://www.youtube.com/embed/rY0WxgSXdEE?si=9wGZ4tzcQW6EDw9_',
-			liked: false,
-			era: '1980s'
-		},
-		{
-			artist: 'Bon Jovi',
-			name: 'Livin’ on a Prayer',
-			src: 'https://www.youtube.com/embed/lDK9QqIzhwk?si=6llbNQzJ6ZLkWQFM',
-			liked: false,
-			era: '1980s'
-		},
-		{
-			artist: 'A-ha',
-			name: 'Take On Me',
-			src: 'https://www.youtube.com/embed/djV11Xbc914?si=hWJtIl0OfV8Tg2KJ',
-			liked: false,
-			era: '1980s'
-		},
-		{
-			artist: 'Cyndi Lauper',
-			name: 'Girls Just Want to Have Fun',
-			src: 'https://www.youtube.com/embed/PIb6AZdTr-A?si=mdP-9u9LwWZcvBGW',
+			artist: 'Phil Collins',
+			name: 'In the Air Tonight',
+			src: 'https://www.youtube.com/embed/YkADj0TPrJA',
 			liked: false,
 			era: '1980s'
 		},
@@ -54,28 +54,33 @@
 		// 🎶 1990s
 
 		{
-			artist: 'Backstreet Boys',
-			name: 'I Want It That Way',
-			src: 'https://www.youtube.com/embed/4fndeDfaWCg?si=4odxE0It7sRzyzC4',
+			artist: 'The Police',
+			name: 'Every Breath You Take',
+			src: 'https://www.youtube.com/embed/OMOGaugKpzs',
 			liked: false,
-			era: '1990s'
+			era: '1980s'
 		},
 		{
-			artist: 'Spice Girls',
-			name: 'Wannabe',
-			src: 'https://www.youtube.com/embed/gJLIiF15wjQ?si=3W2e7jLPx1OOPZgr',
+			artist: 'Michael Jackson',
+			name: 'Smooth Criminal',
+			src: 'https://www.youtube.com/embed/h_D3VFfhvs4',
 			liked: false,
-			era: '1990s'
+			era: '1980s'
 		},
 		{
-			artist: 'Red Hot Chili Peppers',
-			name: 'Californication',
-			src: 'https://www.youtube.com/embed/YlUKcNNmywk?si=KmgH1IRmZjEjEY0o',
+			artist: 'Coolio',
+			name: "Gangsta's Paradise",
+			src: 'https://www.youtube.com/embed/fPO76Jlnz6c',
 			liked: false,
 			era: '1990s'
 		}
 	];
 </script>
+
+<h1 class="mb-5">
+	You can vote any songs that you want to have in the home page by clicking thumbs up or thumbs
+	down.
+</h1>
 
 <div class="grid grid-cols-2 gap-2">
 	{#each arr as v, i}
@@ -102,11 +107,19 @@
 			<div class="mt-1 flex justify-end">
 				<button
 					class="btn btn-sm"
-					class:btn-success={v.liked}
-					class:btn-primary={!v.liked}
-					onclick={() => (v.liked = !v.liked)}
+					class:btn-success={v.thumbsDown}
+					class:btn-primary={!v.thumbsDown}
+					onclick={() => (v.thumbsDown = !v.thumbsDown)}
 				>
-					{v.liked ? 'LIKED' : 'LIKE'}
+					{v.thumbsDown ? 'thumbs up!' : 'thumbs up?'}
+				</button>
+				<button
+					class="btn btn-sm"
+					class:btn-success={v.loved}
+					class:btn-primary={!v.loved}
+					onclick={() => (v.loved = !v.loved)}
+				>
+					{v.loved ? 'thumbs down!' : 'thumbs down?'}
 				</button>
 			</div>
 		</div>
