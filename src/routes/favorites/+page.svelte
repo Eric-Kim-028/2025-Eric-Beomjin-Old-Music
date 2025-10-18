e6fdafa4618a004545571b8085ab999c52557d9d
Eric-Kim-028 <2011erickim@gmail.com>
2025-10-18 01:03:04 +0000

    done

--- src/routes/favorites/+page.svelte
+++ src/routes/favorites/+page.svelte
@@ -4,37 +4,43 @@
 		{
 			artist: 'Michael Jackson',
 			name: 'Billie Jean',
-			src: 'https://www.youtube.com/embed/Zi_XLOBDo_Y?si=Zt1FPG8Q9QKX3FJg',
 			liked: false,
 			era: '1980s'
 		},
 		{
 			artist: 'Nirvana',
 			name: 'Smells Like Teen Spirit',
-			src: 'https://www.youtube.com/embed/hTWKbfoikeg?si=2azDHQiaIG7kSB6y',
 			liked: false,
 			era: '1990s'
 		},
 		{
 			artist: 'Oasis',
 			name: 'Wonderwall',
-			src: 'https://www.youtube.com/embed/bx1Bh8ZvH84?si=7D5xZyN3GtxGrxkF',
 			liked: false,
 			era: '1990s'
 		},
 		{
 			artist: 'Queen',
 			name: 'Another One Bites the Dust',
-			src: 'https://www.youtube.com/embed/rY0WxgSXdEE?si=9wGZ4tzcQW6EDw9_',
 			liked: false,
 			era: '1980s'
 		}
 	];
 </script>
 
-<div class="grid grid-cols-2 gap-2">
+<iframe
+	width="100%"
+	height="300"
+	src={'https://www.youtube.com/embed/Zi_XLOBDo_Y?si=Zt1FPG8Q9QKX3FJg'}
+	title="YouTube video player"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	referrerpolicy="strict-origin-when-cross-origin"
+	allowfullscreen
+></iframe>
+<div class="grid grid-cols-1 gap-2">
 	{#each arr as v, i}
-		<div class="rounded-lg bg-base-300 p-2">
+		<div class="flex justify-evenly rounded-lg bg-base-300 p-2">
 			<div
 				class="badge"
 				class:badge-info={v.era === '1980s'}
@@ -43,10 +49,10 @@
 				{v.era}..
 			</div>
 			<div>{v.artist}</div>
-			<div>{v.name.substr(0, 20)}...</div>
+			<div>{v.name.substr(0, 1000)}</div>
 			<iframe
-				width="100%"
-				height="120"
+				width="50%"
+				height="1"
 				src={v.src}
 				title="YouTube video player"
 				frameborder="0"
@@ -54,7 +60,7 @@
 				referrerpolicy="strict-origin-when-cross-origin"
 				allowfullscreen
 			></iframe>
-			<div class="mt-1 flex justify-end">
+			<div class="mt-1 flex justify-start">
 				<button
 					class="btn btn-sm"
 					class:btn-success={v.liked}
@@ -64,6 +70,14 @@
 					{v.liked ? 'delete' : 'delete'}
 				</button>
 			</div>
+			<button
+				class="btn btn-sm"
+				class:btn-success={v.played}
+				class:btn-primary={!v.played}
+				onclick={() => (v.played = !v.played)}
+			>
+				{v.played ? 'playing' : 'play'}
+			</button>
 		</div>
 	{/each}
 </div>
